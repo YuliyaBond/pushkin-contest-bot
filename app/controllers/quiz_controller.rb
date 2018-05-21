7c2b9820349272bfaa0c3f55e4a0e283d74c9f9f
YuliyaBond <y.d.bondareva@inbox.ru>
2018-05-21 13:19:29 +0300

    level 5

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -3,7 +3,7 @@ require_relative '../services/pushkin'
 
 class QuizController < ApplicationController
   skip_before_action :verify_authenticity_token  
-  FILE_POEMS = FilePushkin.new.parse
+  PUSHKIN_POEMS = FilePushkin.new.parse
 
   def index 
     
@@ -21,6 +21,8 @@ class QuizController < ApplicationController
       answer = level_3(params[:question])
     when 4
       answer = level_4(params[:question])
+    when 5
+      answer = level_5(params[:question])
     end
 
     uri = URI("http://pushkin.rubyroidlabs.com/quiz")
@@ -37,7 +39,7 @@ class QuizController < ApplicationController
 
   def level_1(question)
     question = del(question)
-    FILE_POEMS.each do |poem|
+    PUSHKIN_POEMS.each do |poem|
       poem[1].each do |line|
         line = del(line)
         return poem[0] if line == question
@@ -47,7 +49,7 @@ class QuizController < ApplicationController
 
   def level_2(question)
     parts = question.split('%')
-    FILE_POEMS.each do |poem|
+    PUSHKIN_POEMS.each do |poem|
       poem[1].each do |line|
         if line.include? parts[0]
           return line.split(' ') - question.split(' ')
@@ -60,13 +62,13 @@ class QuizController < ApplicationController
     parts = question.split("\n")
     str_1 = parts[0].split('%')
     
-    FILE_POEMS.each do |poem|
+    PUSHKIN_POEMS.each do |poem|
       poem[1].each do |line|
         if line.include? str_1[0]
           word_1 =  line.split(' ') - parts[0].split(' ')
-          ind_1 = poem[1].index(line)           
-          ind_2 =  poem[1][ind_1 + 1]
-          word_2 = ind_2.split(' ') - parts[1].split(' ')
+          index_1 = poem[1].index(line)           
+          index_2 =  poem[1][index_1 + 1]
+          word_2 = index_2.split(' ') - parts[1].split(' ')
           @str = "#{word_1[0]},#{word_2[0]}"
           break
         end       
@@ -79,15 +81,15 @@ class QuizController < ApplicationController
     parts = question.split("\n")
     str_1 = parts[0].split('%')
     
-    FILE_POEMS.each do |poem|
+    PUSHKIN_POEMS.each do |poem|
       poem[1].each do |line|
         if line.include? str_1[0]
           word_1 =  line.split(' ') - parts[0].split(' ')
-          ind_1 = poem[1].index(line)           
-          ind_2 =  poem[1][ind_1 + 1]
-          ind_3 = poem[1][ind_1 + 2]
-          word_2 = ind_2.split(' ') - parts[1].split(' ')
-          word_3 = ind_3.split(' ') - parts[2].split(' ')
+          index_1 = poem[1].index(line)           
+          index_2 =  poem[1][index_1 + 1]
+          index_3 = poem[1][index_1 + 2]
+          word_2 = index_2.split(' ') - parts[1].split(' ')
+          word_3 = index_3.split(' ') - parts[2].split(' ')
           @str = "#{word_1[0]},#{word_2[0]},#{word_3[0]}"
           break
         end       
@@ -96,6 +98,17 @@ class QuizController < ApplicationController
     return @str
   end
 
+  def level_5(question)
+    parts = question.split(' ')
+    FILE_POEMS.each do |poem|
+      poem[1].each do |line|
+        if line.include? parts[0]
+          word_1 = line.split(' ') - question.split(' ')
+          word_2 = question.split(' ') - line.split(' ')
+          @str = "#{word_1[0]},#{word_2[0]}"
+          break
+        end
+      end
   
   
   def del(str)
